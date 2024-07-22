60f9e9396823ffe57c0580bff43ca2ed53862d7c
kruq <kruq@kruq.myguest.virtualbox.org>
2024-07-22 22:28:27 +0200

    Remove todo

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,7 +5,6 @@
 #
 ##############################################################
 
-#TODO: Fill up the contents below in order to reference your assignment 3 git contents
 AESD_ASSIGNMENTS_VERSION = e0fd4f53f87fa753dfa8b06179bbfa54b2d73fd1
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
