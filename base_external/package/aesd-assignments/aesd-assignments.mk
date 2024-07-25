05d66ef20518f38d78ca969236fecf528430bad8
kruq <kruq@kruq.myguest.virtualbox.org>
2024-07-25 20:46:14 +0200

    Writer

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -14,7 +14,7 @@ AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app writer
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
