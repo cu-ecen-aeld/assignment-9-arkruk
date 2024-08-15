4485416fcda8d6c97ea2ada508ebf53aa106292b
kruq <kruq@kruq.myguest.virtualbox.org>
2024-08-15 15:28:53 +0200

    install script

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -1,33 +1,24 @@
 
 ##############################################################
 #
-# AESD-ASSIGNMENTS
+# LDD
 #
 ##############################################################
 
-LDD_VERSION = f5019f866887ed84a1d47555b6c3a450a11c462d
-# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly  $(MAKE) -C $(@D)/misc-modules modules
-#	$(MAKE) -C $(@D)/scull modules
-# with ssh keys and the automated build/test system.
-# Your site should start with git@github.com:
-#    $(MAKE) -C '$(@D)/misc-modules' LINUX_DIR='$(LINUX_DIR)' CC='$(TARGET_CC)' LD='$(TARGET_LD)' modules
-#	$(MAKE) -C '$(@D)/scull' LINUX_DIR='$(LINUX_DIR)' CC='$(TARGET_CC)' LD='$(TARGET_LD)' modules
+LDD_VERSION = cc6c13de47d13be1bd95c3118b31931d004c85ac
 LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-arkruk.git
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
 
-define LDD_BUILD_CMDS
-    $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/misc-modules modules
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/scull modules
-endef
+LDD_MODULE_SUBDIRS = scull misc-modules
+LDD_MODULE_MAKE_OPTS = KVERSION=$(LINUX_VERSION_PROBED)
 
 define LDD_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/usr/bin
- 	$(INSTALL) -m 0755 $(@D)/scull/scull.ko $(TARGET_DIR)/lib/modules
-	$(INSTALL) -m 0755 $(@D)/misc-modules/hello.ko $(TARGET_DIR)/lib/modules
+	$(INSTALL) -m 0755 $(@D)/modules-start-stop $(TARGET_DIR)/etc/init.d/S98lddmodules
 endef
 
 $(eval $(kernel-module))
